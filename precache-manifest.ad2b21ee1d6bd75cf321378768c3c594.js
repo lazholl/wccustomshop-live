2bc57b9e407b626bc63583eb2e81bf4ba6266831
lazholl <70076938+lazholl@users.noreply.github.com>
2020-10-05 21:28:26 -0700

    Add files via upload

---
+++ precache-manifest.ad2b21ee1d6bd75cf321378768c3c594.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b937093e671cb75e88a571b0c3f9113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9568e9c9bb180fdb733",
+    "url": "/static/css/main.880115ec.chunk.css"
+  },
+  {
+    "revision": "82eba4872511f9e36b54",
+    "url": "/static/js/2.317b82b4.chunk.js"
+  },
+  {
+    "revision": "c9568e9c9bb180fdb733",
+    "url": "/static/js/main.9d550749.chunk.js"
+  },
+  {
+    "revision": "f15498e6466b2ac19ca7",
+    "url": "/static/js/runtime~main.9b3571f9.js"
+  },
+  {
+    "revision": "353c3fb5fdd8f7b8a66ce896becf3c57",
+    "url": "/static/media/analogshirt.353c3fb5.jpg"
+  },
+  {
+    "revision": "b08cd14bbbe6eeb62ecbfd710f9fdab2",
+    "url": "/static/media/ballsilver.b08cd14b.jpg"
+  },
+  {
+    "revision": "8f92cff3f48a6e918c21e01de450ea68",
+    "url": "/static/media/biasknob.8f92cff3.png"
+  },
+  {
+    "revision": "4abb016a717e8f9d4bc9eefd59bbfc87",
+    "url": "/static/media/blackshirt.4abb016a.jpg"
+  },
+  {
+    "revision": "592bada8b7d9168006a9274304c8128b",
+    "url": "/static/media/boxofwar.592bada8.png"
+  },
+  {
+    "revision": "fb8a9355b2f6fb0c544173141073987c",
+    "url": "/static/media/boxofwar1.fb8a9355.jpg"
+  },
+  {
+    "revision": "50b184d27ff45ddf6b9cd3dc30f42e44",
+    "url": "/static/media/boxofwar2.50b184d2.jpg"
+  },
+  {
+    "revision": "5ecaad5683071993f44fc117e2097038",
+    "url": "/static/media/caprid.5ecaad56.png"
+  },
+  {
+    "revision": "0db117eae60f64ab6b951c2fb11655f4",
+    "url": "/static/media/caprid1.0db117ea.jpg"
+  },
+  {
+    "revision": "206553b866e96e8e37ddee3daf828ead",
+    "url": "/static/media/caprid1A.206553b8.jpeg"
+  },
+  {
+    "revision": "040b4de62f9ede7cd3362d1fd62ea1d0",
+    "url": "/static/media/caprid1Ag2.040b4de6.jpg"
+  },
+  {
+    "revision": "bbe9e5be0ad1faec5672db1d485db25d",
+    "url": "/static/media/caprid2.bbe9e5be.jpg"
+  },
+  {
+    "revision": "0e39298d842712fbb1a4164402fd37f6",
+    "url": "/static/media/creamsicle.0e39298d.png"
+  },
+  {
+    "revision": "6af4bf5efb6760c00df8726118cbc1d6",
+    "url": "/static/media/hoodie.6af4bf5e.jpg"
+  },
+  {
+    "revision": "5200f0d58229df8f5f155c1e0cebdc86",
+    "url": "/static/media/logo.5200f0d5.png"
+  },
+  {
+    "revision": "17a1407c2c5ac45dca47677154df7049",
+    "url": "/static/media/orange-bow.17a1407c.png"
+  },
+  {
+    "revision": "34c4d552c36276d85a68a9bb42396e20",
+    "url": "/static/media/popup.34c4d552.jpg"
+  },
+  {
+    "revision": "0ee6be7b65a6662604b48584242e57e8",
+    "url": "/static/media/popup2.0ee6be7b.jpg"
+  },
+  {
+    "revision": "ef33043392387d4476562812fd049c82",
+    "url": "/static/media/psu.ef330433.png"
+  },
+  {
+    "revision": "2ddb6a96eb2e99e0987904bae8d2d8bd",
+    "url": "/static/media/psupos.2ddb6a96.png"
+  },
+  {
+    "revision": "ddc75211800d6d70da94c79c8a51bcd6",
+    "url": "/static/media/resistors.ddc75211.png"
+  },
+  {
+    "revision": "243d9707b015e5c7c495196d329e2b49",
+    "url": "/static/media/resistshirt.243d9707.jpg"
+  },
+  {
+    "revision": "cc547a9a625e4b9eb5a2ee154f6862c2",
+    "url": "/static/media/resistshirtback.cc547a9a.jpg"
+  },
+  {
+    "revision": "b859c96e59023eacc4248d95160cd2ae",
+    "url": "/static/media/restorations.b859c96e.jpg"
+  },
+  {
+    "revision": "01e1056780ea4eb369253f66862013f7",
+    "url": "/static/media/restore1.01e10567.jpg"
+  },
+  {
+    "revision": "2ea51b28b2f20dc7c75b115c0962d096",
+    "url": "/static/media/restore2.2ea51b28.jpg"
+  },
+  {
+    "revision": "3e9bbccbadd8807753f9ba78039e7e40",
+    "url": "/static/media/restore3.3e9bbccb.jpg"
+  },
+  {
+    "revision": "fbf2a4fb8396d480e86f4f893542ac86",
+    "url": "/static/media/straplocks.fbf2a4fb.jpg"
+  },
+  {
+    "revision": "825d5fb350934836aa2ec08771a40783",
+    "url": "/static/media/stuartshirt.825d5fb3.jpg"
+  },
+  {
+    "revision": "d9a4e8201deb9d95fced2af97ed9ae7e",
+    "url": "/static/media/superrussian.d9a4e820.jpg"
+  },
+  {
+    "revision": "455b353e083d9b7ecd6964e874ac3df9",
+    "url": "/static/media/tallfont.455b353e.png"
+  },
+  {
+    "revision": "41b4b2c9261e99e45910c3bb5640bf28",
+    "url": "/static/media/tallfont1.41b4b2c9.jpg"
+  },
+  {
+    "revision": "e5e41613e5cba205164240c9ff58cd5f",
+    "url": "/static/media/tallfont2.e5e41613.jpg"
+  },
+  {
+    "revision": "f19291c025c84ade9bd04bf82407b329",
+    "url": "/static/media/toggle.f19291c0.png"
+  },
+  {
+    "revision": "607d3df553c0f20bdd1c9623f4429d62",
+    "url": "/static/media/toggleleft.607d3df5.png"
+  },
+  {
+    "revision": "b78a58b77dcc824afa342cc3ff6b404f",
+    "url": "/static/media/toggleright.b78a58b7.png"
+  },
+  {
+    "revision": "9f04a22f6e65dee398fd8a1abe68cdb6",
+    "url": "/static/media/twofive.9f04a22f.jpeg"
+  },
+  {
+    "revision": "ae7742673d92ac634cabe2e9f1d6d6d6",
+    "url": "/static/media/ufoshirt.ae774267.jpg"
+  },
+  {
+    "revision": "7890e379fac8d048042fb7b020088d8a",
+    "url": "/static/media/white.7890e379.png"
+  },
+  {
+    "revision": "6dd19ad487671d0d05d070afa51de466",
+    "url": "/static/media/whiteelk.6dd19ad4.png"
+  },
+  {
+    "revision": "16b8408ea40e3b5271b9a03c4f762c93",
+    "url": "/static/media/whiteelk1.16b8408e.jpg"
+  },
+  {
+    "revision": "26b25aa6b69128ab140bc8a8c5c6129d",
+    "url": "/static/media/whiteelk2.26b25aa6.jpg"
+  },
+  {
+    "revision": "ba86fa7868d5a3fa99b8c65f1cc1569d",
+    "url": "/static/media/yourface60s.ba86fa78.png"
+  },
+  {
+    "revision": "5e2141624424fe8fec5f662cc0c72683",
+    "url": "/static/media/yourface70s.5e214162.png"
+  },
+  {
+    "revision": "6eda0a5eb5dfc14ead394aa94f942d60",
+    "url": "/static/media/zincbatt.6eda0a5e.png"
+  }
+]);
